c9f9b52e7852b8581f9d1aabef22670b3a938390
Chris Rowles <c.rowles@sentrybay.com>
2023-08-01 14:48:01 +0100

    All

--- app/systems/components/system-detail.tsx
+++ app/systems/components/system-detail.tsx
@@ -3,7 +3,7 @@
 
 import { usePathname } from 'next/navigation';
 import { FunctionComponent, useEffect, useState } from 'react';
-import { System, SystemCelestial as ISystemCelestial } from '../../lib/interfaces/System';
+import { System, SystemCelestial as ISystemCelestial, MappedSystemCelestial } from '../../lib/interfaces/System';
 import { Schedule } from '../../lib/interfaces/Schedule';
 import { Pagination } from '../../lib/interfaces/Pagination';
 import { systemState } from '../lib/systems';
@@ -37,7 +37,6 @@ const SystemDetail: FunctionComponent = () => {
       }).then((system) => {
         setSystem(system);
         const map = new SystemMap(system);
-        console.log({ map });
         setSystemMap(map);
 
         getCollection<Schedule>('fleet/schedule', {
@@ -57,61 +56,61 @@ const SystemDetail: FunctionComponent = () => {
     }
   }, [slug]);
 
-  function renderMainStar(celestials: ISystemCelestial[]) {
+  function renderCelestial(celestial: MappedSystemCelestial) {
     return (
-      <SystemCelestial key={celestials[0].id64}
-        id={celestials[0].id64}
-        celestial={celestials[0]}
+      <SystemCelestial key={celestial.id64}
+        id={celestial.id64}
         system={system.name}
-        main={true}
-        orbiting={(celestials.length-1)}
+        celestial={celestial as ISystemCelestial}
+        main={!!celestial.is_main_star}
+        orbiting={(celestial._children.length)}
         dispatcher={systemDispatcher}
         className="w-32 text-glow-white text-sm" />
     );
   }
 
-  function renderCelestials(
-    celestials: ISystemCelestial[],
-    {slice, filter, level}: {slice?: number[] | null, filter?: 'Planet' | 'Star', level?: number}
-  ) {
-    const [start, end] = slice ?? [0, celestials.length];
+  function renderCelestialChildren(celestial: MappedSystemCelestial) {
+    const celestials = celestial._children.length > 0
+      ? celestial._children
+      : false;
 
-    return celestials.filter((c: ISystemCelestial) => {
-      return (filter ? c.type === filter : true)
-        && (level ? c.parents && c.parents.length === level : true);
-    }).slice(start, end).map((celestial: ISystemCelestial) => {
-      return (
-        <SystemCelestial key={celestial.id64}
-          id={celestial.id64}
-          celestial={celestial}
-          system={system.name}
-          dispatcher={systemDispatcher}
-          className="w-32 text-glow-white text-sm" />
-      );
-    });
+      if (! celestials) {
+        return <>Celestial telemetry data not found for {system.name}</>
+      }
+
+      return celestials.map((celestial: MappedSystemCelestial) => renderCelestial(celestial));
+  }
+
+  function renderMappedCelestials(map: SystemMap) {
+    const star = map.stars.find(s => !!s.is_main_star);
+
+    return (
+      <>
+        <div className="flex items-center content-center gap-4">
+          {star && <>
+            <div className="border-r pe-12 border-neutral-700 rounded-full">
+              {renderCelestial(star)}
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="text-xs">L1 Orbitals</span>
+              {renderCelestialChildren(star)}
+            </div>
+          </>}
+        </div>
+      </>
+    )
   }
 
   return (
     <>
       {isLoading && <Loader visible={isLoading} />}
       <div className="pb-3 border-b border-neutral-800">
-        <SystemTitle title={system.name}
-          celestials={system.bodies.length}/>
+        <SystemTitle title={system.name} celestials={system.bodies.length}/>
       </div>
       <SystemInformation coords={system.coords} information={system.information} />
       <div className="py-10 w-7xl overflow">
         <Heading icon="icarus-terminal-system-bodies" title="System Bodies" className="gap-2 pb-5" />
-        {!isLoading && system.bodies && system.bodies.length > 0 ?
-        <div className="flex items-center content-center gap-4">
-          <div className="border-r pe-12 border-neutral-700 rounded-full">{renderMainStar(system.bodies)}</div>
-          <div className="flex items-center gap-3">
-            <span className="text-xs">L1 Orbitals</span>
-            {renderCelestials(system.bodies, {
-              level: 1,
-              slice: [0, 8]
-            })}
-          </div>
-        </div>
+        {!isLoading && systemMap ? renderMappedCelestials(systemMap)
         : <div>No celestial bodies found in this system...</div>}
       </div>
       <div className="py-5">
