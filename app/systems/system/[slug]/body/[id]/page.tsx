aa50a1a2c1d803201b7b2e6724e4505fcd0e689c
Chris Rowles <c.rowles@sentrybay.com>
2024-08-17 22:24:13 +0100

    fix error

--- app/systems/system/[slug]/body/[id]/page.tsx
+++ app/systems/system/[slug]/body/[id]/page.tsx
@@ -1,11 +1,11 @@
 import Heading from "../../../../../components/heading";
 import SystemBodyPage from "../../../../components/system-body-page";
 
-export default async function Page() {
+export default async function Page({params}: { params: { slug: string } }) {
   return (
     <>
       <Heading title="Body Information" className="mb-5 gap-2" />
-      <SystemBodyPage />
+      <SystemBodyPage params={params} />
     </>
   );
 }
