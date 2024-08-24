0bd453aeb386d70a670ea7b985de4b0803a8cf9b
Chris Rowles <c.rowles@sentrybay.com>
2024-08-24 21:40:56 +0100

    add audio

--- app/galnet/components/galnet-ticker.tsx
+++ app/galnet/components/galnet-ticker.tsx
@@ -10,11 +10,13 @@ interface Props {
 
 const NewsTicker: FunctionComponent<Props> = ({ headlines }) => {
   const [currentTime, setCurrentTime] = useState("00:00");
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const [currentHeadlineIndex, setCurrentHeadlineIndex] = useState(0);
 
   const tickerContentRef = useRef<HTMLDivElement>(null);
   const tickerRef = useRef<HTMLDivElement>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
     const updateClock = () => {
@@ -94,10 +96,58 @@ const NewsTicker: FunctionComponent<Props> = ({ headlines }) => {
     };
   }, [currentHeadlineIndex, headlines]);
 
+  const togglePlayPause = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="flex items-center">
       <span className="text-glow__orange border-b-glow__orange ticker-label bg-black/60 px-4 text-sm text-xs uppercase">
-        Galnet <span className="ms-2">{currentTime} UTC</span>
+        <span>Galnet</span>
+        <span className="ms-2">{currentTime} UTC</span>
+        <button
+          onClick={togglePlayPause}
+          className="ml-4 flex items-center justify-center rounded-full bg-black/60 p-2"
+          aria-label={isPlaying ? "Pause" : "Play"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-3 w-3 ${isPlaying ? "hidden" : "block"}`}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 3l14 9-14 9V3z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-3 w-3 ${isPlaying ? "block" : "hidden"}`}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M6 19h2V5H6zm10-14h2v14h-2z" />
+          </svg>
+        </button>
+        <audio ref={audioRef} className="hidden">
+          <source src="/audio/galnet-1.mp3" type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
       </span>
       <div
         ref={tickerRef}
