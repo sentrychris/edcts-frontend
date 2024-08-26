ba3e1e72127855478ba50d41e6640d960ecd40c1
Chris Rowles <c.rowles@sentrybay.com>
2024-08-26 13:53:27 +0100

    add galaxy map changes

--- app/galnet/components/galnet-ticker.tsx
+++ app/galnet/components/galnet-ticker.tsx
@@ -99,7 +99,7 @@ const NewsTicker: FunctionComponent<Props> = ({ articles }) => {
   const currentDate = getCurrentEliteDate();
 
   return (
-    <div className="flex items-center">
+    <div className="relative z-30 flex items-center">
       <span className="text-glow__orange border-b-glow__orange ticker-label lg:px-18 bg-black/60 px-6 text-sm text-xs uppercase md:px-12">
         <span>Galnet</span>
         <span className="ms-2 hidden sm:flex">
