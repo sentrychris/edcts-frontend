472f9323dc1867835935990b0f320545fc87b788
Chris Rowles <c.rowles@sentrybay.com>
2024-09-11 10:57:34 +0100

    Re-add EDSM check

--- app/stations/components/station-header.tsx
+++ app/stations/components/station-header.tsx
@@ -1,6 +1,7 @@
 import type { FunctionComponent } from "react";
 import type { Station } from "@/core/interfaces/Station";
 import { stationIconByType } from "@/app/systems/lib/render-utils";
+import Link from "next/link";
 
 interface Props {
   station: Station;
@@ -13,8 +14,14 @@ const StationHeader: FunctionComponent<Props> = ({ station }) => {
         <i className={stationIconByType(station.type)} style={{ fontSize: "3rem" }}></i>
         <div>
           <h2 className="text-3xl">
-            {station.name} - {station.type}
+            {station.name}
           </h2>
+          <h4>
+            <span className="text-glow">{station.type}</span>
+            <Link className="ms-3 text-glow__orange hover:text-glow__blue" href={station.system?.slug ?? '#'}>
+              {station.system?.name} system
+            </Link>
+          </h4>
         </div>
       </div>
     </div>
