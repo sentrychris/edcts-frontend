60ff0016e0f62abde93157e386fdf2819c9e2565
christopher rowles <c.rowles@sentrybay.com>
2026-04-11 17:19:07 +0100

    Flush stat cache

--- app/systems/components/systems-statistics-bar.tsx
+++ app/systems/components/systems-statistics-bar.tsx
@@ -59,14 +59,14 @@ const SystemsStatisticsBar: FunctionComponent<Props> = ({
     },
     {
       icon: "icarus-terminal-scan",
-      label: "EDDN Listener",
+      label: "EDDN",
       value: <span className="text-green-400 uppercase text-sm">
         online
       </span>,
     },
     {
       icon: "icarus-terminal-system-bodies",
-      label: "EDSM API",
+      label: "EDSM",
       value: <span className="text-green-400 uppercase text-sm">
         Online
       </span>,
